9f71b9cee75a9bf00f98c9edfd8c84ce998305fe
Игорь Токарев <tokarev@id317.ru>
2019-01-24 23:05:51 +0500

    update

--- lab3/lab.cpp
+++ lab3/lab.cpp
@@ -9,14 +9,12 @@ int main(int argc, char *argv[]){
     long long q = 5; //простое
     long long X = 112; //факторизуемое
     long long g = 7;
+    long long x = 2;
+    int m = 1;
 
     std::srand(unsigned(std::time(0)));
     int random_variable = std::rand();
     
-    std::cout << "Случайное значение на отрезке [0 " << RAND_MAX << "]: "
-              << random_variable << '\n';
-    float rounded = round((float)random_variable/RAND_MAX);
-    std::cout << "round(): " << rounded << '\n';
 
     std::cout << "p: " << p << std::endl;
     std::cout << "q: " << q << std::endl;
@@ -29,10 +27,37 @@ int main(int argc, char *argv[]){
      * get 'r' from Multiplicative group of integers modulo q
      * getting h= g^r(mod p);
      */
+    std::cout << "First step by Prover: " << std::endl;
     long long r = round(((float)random_variable/RAND_MAX)*(q-1));
     std::cout << "r: " << r << std::endl;
     long long h = (long long)pow(g, r) % p;
     std::cout << "h: " << h << std::endl;
-    
+    /**
+     * 1. step by V.
+     * getting b = 0 or 1.
+     */
+    std::cout << "First step by Verifier: " << std::endl;
+    long long b = round((float)random_variable/RAND_MAX);
+    std::cout << "b: " << b << '\n';
+
+    /**
+     * 2. step by P.
+     * P must get 'k': k = (r + xb) % q
+     */
+    std::cout << "Second step by Prover: " << std::endl;
+
+    long long k = (r + x*b) % q;
+    std::cout << "k: " << k << std::endl;
+    long long g_k = (long long) pow(g, k);
+    long long X_b_h = ((long long ) pow(X, b) * h) % p;
+    while (m > 0){
+        if (g_k == X_b_h) {
+            std::cout << "is g_k == X_b_h: true" << std::endl;
+        } else {
+            std::cout << "is g_k != X_b_h: false" << std::endl;
+        }
+        m--;
+    }
+
     return 0;
 }
